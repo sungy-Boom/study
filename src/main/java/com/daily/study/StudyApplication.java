f3359364a02392be1d1362c8c98c6c953ccc9fc1
孙桂勇 <985819203@qq.com>
2017-11-23 10:49:59 +0800

    start

---
+++ src/main/java/com/daily/study/StudyApplication.java
@@ -0,0 +1,15 @@
+package com.daily.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author SunGuiyong
+ */
+@SpringBootApplication
+public class StudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyApplication.class, args);
+    }
+}
