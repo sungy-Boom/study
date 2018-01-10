84e81e799b79de40d6d4d41d33eaa12efa603c15
sunguiyong <985819203@qq.com>
2018-01-10 19:35:27 +0800

    prime generate

--- learn/src/main/java/com/daily/learn/PrimeGenerateTest.java
+++ learn/src/main/java/com/daily/learn/PrimeGenerateTest.java
@@ -1,7 +1,10 @@
 package com.daily.learn;
 
+import com.google.common.collect.ImmutableList;
+
 import java.math.BigInteger;
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by SunGuiyong
@@ -11,32 +14,126 @@ public class PrimeGenerateTest {
 
     public static void main(String[] args) {
         PrimeGenerateTest test = new PrimeGenerateTest();
-        int[] res = test.primeGenerate(1000);
-        for (int i = 0; i < res.length; i++) {
-            System.out.print(res[i] + " ");
-            if ((i + 1) % 30 == 0) {
-                System.out.println();
-            }
-        }
+        List<Integer> res = test.generatePrime(1, 1000000);
+        System.out.println(res.size());
+
+        long start;
+        long end;
+
+        int[] primeFactor1 = test.getPrimeFactor(2 * 7 * 13 * 11);
+        start = System.currentTimeMillis();
+        List<Integer> primeFactor = test.getPrimeFactor_list(2 * 23 * 11 * 29 * 31 * 37 * 42);
+        end = System.currentTimeMillis();
+        System.out.println(primeFactor.size());
+        System.out.println("getPrimeFactor_list use list time " + (end - start));
+
+        start = System.currentTimeMillis();
+        System.out.println();
+        List<Integer> list = ImmutableList.of(2, 23, 11, 29, 31, 37, 42);
+        String result = test.getPrimeProduct(list);
+        end = System.currentTimeMillis();
+        System.out.println(result);
+        System.out.println("getPrimeProduct use BigInteger time " + (end - start));
+
+        start = System.currentTimeMillis();
+        List<Integer> divRes = test.getPrimeFactor("706905276");
+        System.out.println(divRes);
+        end = System.currentTimeMillis();
+        System.out.println("getPrimeFactor use bigInteger time " + (end - start));
     }
 
-    private int[] primeGenerate(int lastNum) {
-        int index = 0;
-        int[] res = new int[lastNum];
-        for (int i = 1; i < lastNum; i++) {
+    //    O(n*Math.sqrt(n))--> O(n/2*Math.sqrt(n))
+    private List<Integer> generatePrime(int startNum, int endNum) {
+        List<Integer> resList = new ArrayList<>();
+
+        if (2 > startNum) {
+            startNum = 2;
+        }
+        if (2 == startNum) {
+            resList.add(startNum);
+        }
+        if (0 == startNum % 2) {
+            startNum += 1;
+        }
+        for (int i = startNum; i < endNum; i += 2) {
             if (isPrime(i)) {
-                res[index++] = i;
+                resList.add(i);
             }
         }
-        return res;
+        return resList;
     }
 
+    //    O(Math.sqrt(n))
     private boolean isPrime(int num) {
-        for (int i = 2; i < Math.abs(num); i++) {
+
+        for (int i = 2; i <= Math.sqrt(num); i++) {
             if (num % i == 0) {
                 return false;
             }
         }
         return true;
+    }
+
+    /*计算质因数，使用int[]*/
+    private int[] getPrimeFactor(int num) {
+        int[] res = new int[num];
+        int index = 0;
+        for (int i = 2; i <= num; i++) {
+            if (0 == num % i) {
+                res[index++] = i;
+                num = num / i;
+            }
+        }
+        return res;
+    }
+
+    /*计算质因数，使用List*/
+    public List<Integer> getPrimeFactor_list(int num) {
+        List<Integer> res = new ArrayList<>();
+        for (int i = 2; i <= num; i++) {
+            if (0 == num % i) {
+                res.add(i);
+                num = num / i;
+            }
+        }
+        return res;
+    }
+
+    /*计算质因数，BigInteger*/
+    public List<Integer> getPrimeFactor(String num) {
+        BigInteger flg = new BigInteger(num);
+        List<Integer> res = new ArrayList<>();
+
+        for (int i = 2; ; i++) {
+            BigInteger temp = new BigInteger(i + "");
+            if (new BigInteger(0 + "").equals(flg.mod(temp))) {
+                res.add(i);
+                flg = flg.divide(temp);
+            }
+            if (temp.compareTo(flg) >= 1) {
+                break;
+            }
+        }
+        return res;
+    }
+
+    /*通过BigInteger 获取乘法结果*/
+    private String getPrimeProduct(int num) {
+
+        BigInteger big = new BigInteger("1");
+        return big.multiply(new BigInteger(num + "")).toString();
+    }
+
+    /*通过BigInteger获取乘法结果*/
+    private String getPrimeProduct(List<Integer> list) {
+
+        BigInteger big = new BigInteger("1");
+
+        for (Integer item : list) {
+            big = big.multiply(new BigInteger(item + ""));
+//            System.out.println(big);
+        }
+
+        return big.toString();
     }
 }
