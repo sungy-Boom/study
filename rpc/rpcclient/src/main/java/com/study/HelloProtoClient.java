a8c88b233168a7e33ed1c554d5d50d839cb421c0
sunguiyong <sunguiyong@100tal.com>
2019-04-13 16:16:30 +0800

    阿里云语音识别

--- rpc/rpcclient/src/main/java/com/study/HelloProtoClient.java
+++ rpc/rpcclient/src/main/java/com/study/HelloProtoClient.java
@@ -1,50 +1,50 @@
-package com.study;
-
-import com.study.person.bean.Person;
-import com.study.person.service.SavePersonInfoGrpc;
-import io.grpc.ManagedChannel;
-import io.grpc.ManagedChannelBuilder;
-
-import java.util.concurrent.TimeUnit;
-
-/**
- * Created by SunGuiyong
- * on 2018/3/22.
- */
-public class HelloProtoClient {
-    private final ManagedChannel channel;
-    private final SavePersonInfoGrpc.SavePersonInfoBlockingStub localStub;
-
-    public HelloProtoClient(String host, int port) {
-        channel = ManagedChannelBuilder.forAddress(host, port)
-                .usePlaintext(true)
-                .build();
-
-        //初始化
-        localStub = SavePersonInfoGrpc.newBlockingStub(channel);
-    }
-
-    public void shutdown() throws InterruptedException {
-        channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
-    }
-
-    public void greet(Person.PersonInfo personInfo) {
-        Person.Result result = localStub.savePerson(personInfo);
-        System.out.println("work code :" + result.getCode());
-        System.out.println("other information :" + result.getOtherInfo());
-    }
-
-    public static void main(String[] args) throws InterruptedException {
-        HelloProtoClient client = new HelloProtoClient("127.0.0.1", 50051);
-        for (int i = 0; i < 5; i++) {
-            Person.PersonInfo p = Person.PersonInfo.newBuilder()
-                    .setPersonName("test" + i)
-                    .setAge(i)
-                    .setSex("男")
-                    .setClazz(Person.TheClass.newBuilder()
-                            .setClassName("终极一班")).build();
-            client.greet(p);
-        }
-        client.shutdown();
-    }
-}
+//package com.study;
+//
+//import com.study.person.bean.Person;
+//import com.study.person.service.SavePersonInfoGrpc;
+//import io.grpc.ManagedChannel;
+//import io.grpc.ManagedChannelBuilder;
+//
+//import java.util.concurrent.TimeUnit;
+//
+///**
+// * Created by SunGuiyong
+// * on 2018/3/22.
+// */
+//public class HelloProtoClient {
+//    private final ManagedChannel channel;
+//    private final SavePersonInfoGrpc.SavePersonInfoBlockingStub localStub;
+//
+//    public HelloProtoClient(String host, int port) {
+//        channel = ManagedChannelBuilder.forAddress(host, port)
+//                .usePlaintext(true)
+//                .build();
+//
+//        //初始化
+//        localStub = SavePersonInfoGrpc.newBlockingStub(channel);
+//    }
+//
+//    public void shutdown() throws InterruptedException {
+//        channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+//    }
+//
+//    public void greet(Person.PersonInfo personInfo) {
+//        Person.Result result = localStub.savePerson(personInfo);
+//        System.out.println("work code :" + result.getCode());
+//        System.out.println("other information :" + result.getOtherInfo());
+//    }
+//
+//    public static void main(String[] args) throws InterruptedException {
+//        HelloProtoClient client = new HelloProtoClient("127.0.0.1", 50051);
+//        for (int i = 0; i < 5; i++) {
+//            Person.PersonInfo p = Person.PersonInfo.newBuilder()
+//                    .setPersonName("test" + i)
+//                    .setAge(i)
+//                    .setSex("男")
+//                    .setClazz(Person.TheClass.newBuilder()
+//                            .setClassName("终极一班")).build();
+//            client.greet(p);
+//        }
+//        client.shutdown();
+//    }
+//}
--- rpc/rpcserver/src/main/java/com/study/HelloProtoServer.java
+++ rpc/rpcserver/src/main/java/com/study/HelloProtoServer.java
@@ -1,62 +1,62 @@
-package com.study;
-
-import com.study.person.bean.Person;
-import com.study.person.service.SavePersonInfoGrpc;
-import io.grpc.Server;
-import io.grpc.ServerBuilder;
-import io.grpc.stub.StreamObserver;
-
-import java.io.IOException;
-
-/**
- * Created by SunGuiyong
- * on 2018/3/22.
- */
-public class HelloProtoServer {
-
-    private int port = 50051;
-    private Server server;
-
-    private void start() throws IOException {
-        server = ServerBuilder.forPort(port)
-                .addService(new PersonServiceImpl())
-                .build()
-                .start();
-        System.out.println("service start...");
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            System.err.println("*** shutting down gRPC server since JVM is shutting down");
-            HelloProtoServer.this.stop();
-            System.err.println("*** server shut down");
-        }));
-    }
-
-    private void stop() {
-        if (server != null) {
-            server.shutdown();
-        }
-    }
-
-    // block 一直到退出程序
-    private void blockUntilShutdown() throws InterruptedException {
-        if (server != null) {
-            server.awaitTermination();
-        }
-    }
-
-    public static void main(String[] args) throws IOException, InterruptedException {
-        final HelloProtoServer server = new HelloProtoServer();
-        server.start();
-        server.blockUntilShutdown();
-    }
-
-
-    private class PersonServiceImpl extends SavePersonInfoGrpc.SavePersonInfoImplBase {
-        public void savePerson(Person.PersonInfo personInfo, StreamObserver<Person.Result> result) {
-            System.out.println("person info :" + personInfo.toString());
-            Person.Result res = Person.Result.newBuilder().setCode(200).build();
-            result.onNext(res);
-            result.onCompleted();
-        }
-    }
-
-}
+//package com.study;
+//
+//import com.study.person.bean.Person;
+//import com.study.person.service.SavePersonInfoGrpc;
+//import io.grpc.Server;
+//import io.grpc.ServerBuilder;
+//import io.grpc.stub.StreamObserver;
+//
+//import java.io.IOException;
+//
+///**
+// * Created by SunGuiyong
+// * on 2018/3/22.
+// */
+//public class HelloProtoServer {
+//
+//    private int port = 50051;
+//    private Server server;
+//
+//    private void start() throws IOException {
+//        server = ServerBuilder.forPort(port)
+//                .addService(new PersonServiceImpl())
+//                .build()
+//                .start();
+//        System.out.println("service start...");
+//        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+//            System.err.println("*** shutting down gRPC server since JVM is shutting down");
+//            HelloProtoServer.this.stop();
+//            System.err.println("*** server shut down");
+//        }));
+//    }
+//
+//    private void stop() {
+//        if (server != null) {
+//            server.shutdown();
+//        }
+//    }
+//
+//    // block 一直到退出程序
+//    private void blockUntilShutdown() throws InterruptedException {
+//        if (server != null) {
+//            server.awaitTermination();
+//        }
+//    }
+//
+//    public static void main(String[] args) throws IOException, InterruptedException {
+//        final HelloProtoServer server = new HelloProtoServer();
+//        server.start();
+//        server.blockUntilShutdown();
+//    }
+//
+//
+//    private class PersonServiceImpl extends SavePersonInfoGrpc.SavePersonInfoImplBase {
+//        public void savePerson(Person.PersonInfo personInfo, StreamObserver<Person.Result> result) {
+//            System.out.println("person info :" + personInfo.toString());
+//            Person.Result res = Person.Result.newBuilder().setCode(200).build();
+//            result.onNext(res);
+//            result.onCompleted();
+//        }
+//    }
+//
+//}
